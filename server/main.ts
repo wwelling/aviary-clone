5dab22da8e0785e7b9a039654bcbaa39384423f1
William Welling <william_welling@harvard.edu>
2022-11-08 12:05:38 -0600

    Add logging

--- server/main.ts
+++ server/main.ts
@@ -1,9 +1,43 @@
 import { NestFactory } from '@nestjs/core';
+import { get } from 'env-var';
+import { WinstonModule } from 'nest-winston';
+import * as os from 'os';
+import { format, transports } from 'winston';
+import 'winston-daily-rotate-file';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const logLevel = get('LOG_LEVEL').default('info').asString();
+  const logPath = get('LOG_PATH').default('logs').asString();
+
+  const app = await NestFactory.create(AppModule, {
+    logger: WinstonModule.createLogger({
+      transports: [
+        new transports.DailyRotateFile({
+          level: logLevel,
+          dirname: logPath,
+          filename: `axiom-api-${os.hostname()}-%DATE%.log`,
+          datePattern: 'yyyy-MM-DD',
+          maxSize: '20m',
+          maxFiles: '14d',
+        }),
+        new transports.Console({
+          format: format.simple()
+        }),
+      ],
+      level: logLevel,
+      format: format.combine(
+        format.timestamp({ format: 'MMM-DD-YYYY HH:mm:ss' }),
+        format.align(),
+        format.printf(
+          (info) => `${info.level}: ${[info['timestamp']]}: ${info.message}`,
+        ),
+      ),
+    })
+  });
+
   app.setGlobalPrefix('api');
+
   await app.listen(process.env['PORT'] || 4000);
 }
 
